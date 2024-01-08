a514c3544d055cccf5d5abb1e609484ed0504fd2
undeath-cyber <gabrielxjonathan.12@gmail.com>
2024-01-09 03:00:29 +0700

    feat(models sequelize) : - add model for tabel food

---
+++ models/Food.js
@@ -0,0 +1,72 @@
+// models/Food.js
+
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Food extends Model {
+    static associate(models) {
+      // Di sini Anda dapat menentukan asosiasi dengan model lain jika diperlukan
+      // Contoh: Food.belongsTo(models.Booth);
+    }
+  }
+  Food.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      guid: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      jenis: {
+        allowNull: false,
+        type: DataTypes.ENUM(
+          "sembako",
+          "minuman",
+          "makanan berat",
+          "makanan ringan",
+          "buah buahan",
+          "kue dan roti"
+        ),
+      },
+      jumlah: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      gambar: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      id_booth: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        // Definisi foreign key yang merujuk ke tabel Booths
+        references: {
+          model: "Booths",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Food",
+      tableName: "Foods",
+    }
+  );
+  return Food;
+};
