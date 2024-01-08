3b1cd627294bb4463e8b86a3b4eee31320d120dd
undeath-cyber <gabrielxjonathan.12@gmail.com>
2024-01-09 02:59:01 +0700

    feat(migration food) : - create migration table food

---
+++ migrations/20240108193611-create-foods-table.js
@@ -0,0 +1,77 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Foods", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      guid: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      jenis: {
+        allowNull: false,
+        type: Sequelize.ENUM(
+          "sembako",
+          "minuman",
+          "makanan berat",
+          "makanan ringan",
+          "buah buahan",
+          "kue dan roti"
+        ),
+      },
+      jumlah: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      gambar: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      id_booth: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Booths", // Nama model untuk asosiasi
+          key: "id", // Kolom referensi di model "Booths"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    // Tambahkan indeks untuk id_booth untuk meningkatkan performa query
+    await queryInterface.addIndex("Foods", ["id_booth"]);
+
+    // Menambahkan foreign key constraint pada kolom id_booth di tabel Foods
+    await queryInterface.addConstraint("Foods", {
+      fields: ["id_booth"],
+      type: "foreign key",
+      name: "fk_foods_booths", // Nama constraint
+      references: {
+        table: "Booths",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    // Drop tabel "Foods" jika migrasi di-rollback
+    await queryInterface.dropTable("Foods");
+  },
+};
